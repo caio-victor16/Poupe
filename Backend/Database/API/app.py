5e3c9c2a05fe31a11741622b90506b8162f85f3c
Arthur Teles Braga <telesarthur773@gmail.com>
2026-08-07 22:21:33 -0300

    Add files via upload

--- Backend/Database/API/app.py
+++ Backend/Database/API/app.py
@@ -5,22 +5,21 @@ from app.controllers.gasto_controller import gasto_bp
 from app.controllers.categoria_controller import categoria_bp
 from app.controllers.boleto_controller import boleto_bp
 from app.controllers.alerta_controller import alerta_bp
+from app.controllers.relatorio_controller import relatorio_bp
 from app.controllers.previsao_controller import previsao_bp
+
 
 app = Flask(__name__)
 
+
 app.register_blueprint(usuario_bp)
-
 app.register_blueprint(gasto_bp)
-
 app.register_blueprint(categoria_bp)
-
 app.register_blueprint(boleto_bp)
-
 app.register_blueprint(alerta_bp)
-
+app.register_blueprint(relatorio_bp)
 app.register_blueprint(previsao_bp)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     app.run(debug=True)
