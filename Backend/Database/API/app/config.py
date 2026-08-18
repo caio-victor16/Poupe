29f18857c937538d25f4ca5905bb31e6f146dd53
caio-victor16 <caiovictorgonzaga2009@gmail.com>
2026-08-18 10:33:30 -0300

    Update config.py

--- Backend/Database/API/app/config.py
+++ Backend/Database/API/app/config.py
@@ -1,28 +1,17 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Config:
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "poupe")
 
-    HOST = os.getenv(
-        "MYSQL_HOST",
-        "localhost"
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@{MYSQL_HOST}/{MYSQL_DATABASE}"
     )
-
-    USER = os.getenv(
-        "MYSQL_USER",
-        "root"
-    )
-
-    PASSWORD = os.getenv(
-        "MYSQL_PASSWORD",
-        ""
-    )
-
-    DATABASE = os.getenv(
-        "MYSQL_DATABASE",
-        "poupe"
-    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
