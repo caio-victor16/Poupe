2c7ef5e22e9b6c8b2e1ee2c1b6c83bbec96adf6c
caio-victor16 <caiovictorgonzaga2009@gmail.com>
2026-08-18 09:06:54 -0300

    Update categoria_service.py

--- Backend/Database/API/app/services/categoria_service.py
+++ Backend/Database/API/app/services/categoria_service.py
@@ -4,25 +4,20 @@ from app.repositories.categoria_repository import CategoriaRepository
 class CategoriaService:
 
     def __init__(self):
-
         self.repository = CategoriaRepository()
 
     def listar(self):
-
-        return self.repository.listar()
+        return [c.to_dict() for c in self.repository.listar()]
 
     def buscar_por_id(self, id_categoria):
-
-        return self.repository.buscar_por_id(id_categoria)
+        categoria = self.repository.buscar_por_id(id_categoria)
+        return categoria.to_dict() if categoria else None
 
     def inserir(self, categoria):
-
         self.repository.inserir(categoria)
 
     def atualizar(self, categoria):
-
         self.repository.atualizar(categoria)
 
     def excluir(self, id_categoria):
-
         self.repository.excluir(id_categoria)
