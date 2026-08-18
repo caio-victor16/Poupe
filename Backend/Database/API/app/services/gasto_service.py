e3d218955aff29000e95b8ed002c39cdb47d6d8a
caio-victor16 <caiovictorgonzaga2009@gmail.com>
2026-08-18 09:06:14 -0300

    Update gasto_service.py

--- Backend/Database/API/app/services/gasto_service.py
+++ Backend/Database/API/app/services/gasto_service.py
@@ -4,37 +4,31 @@ from app.repositories.gasto_repository import GastoRepository
 class GastoService:
 
     def __init__(self):
-
         self.repository = GastoRepository()
 
     def listar(self):
-
-        return self.repository.listar()
+        return [g.to_dict() for g in self.repository.listar()]
 
     def buscar_por_id(self, id_gasto):
-
-        return self.repository.buscar_por_id(id_gasto)
+        gasto = self.repository.buscar_por_id(id_gasto)
+        return gasto.to_dict() if gasto else None
 
     def inserir(self, gasto):
-
         self.repository.inserir(gasto)
 
     def atualizar(self, gasto):
-
         self.repository.atualizar(gasto)
 
     def excluir(self, id_gasto):
-
         self.repository.excluir(id_gasto)
 
     def gastos_por_categoria(self, id_usuario):
-
         return self.repository.gastos_por_categoria(id_usuario)
-    
-    def gastos_por_periodo(
-        self, id_usuario, data_inicio, data_fim
-    ):
-        return self.repository.gastos_por_periodo(id_usuario, data_inicio,data_fim)
-    
+
+    def gastos_por_periodo(self, id_usuario, data_inicio, data_fim):
+        return self.repository.gastos_por_periodo(
+            id_usuario, data_inicio, data_fim
+        )
+
     def verificar_limite(self, id_usuario):
         return self.repository.verificar_limite(id_usuario)
