e66b7dcf044cabf01131d7e58ac89c11061a812e
caio-victor16 <caiovictorgonzaga2009@gmail.com>
2026-08-18 09:05:30 -0300

    Update alerta_service.py

--- Backend/Database/API/app/services/alerta_service.py
+++ Backend/Database/API/app/services/alerta_service.py
@@ -4,35 +4,26 @@ from app.repositories.alerta_repository import AlertaRepository
 class AlertaService:
 
     def __init__(self):
-
         self.repository = AlertaRepository()
 
     def listar_por_usuario(self, id_usuario):
+        return [
+            a.to_dict()
+            for a in self.repository.listar_por_usuario(id_usuario)
+        ]
 
-        return self.repository.listar_por_usuario(
-            id_usuario
-        )
-
-    def buscar_por_id(self, id_alert):
-
-        return self.repository.buscar_por_id(
-            id_alert
-        )
+    def buscar_por_id(self, id_alerta):
+        alerta = self.repository.buscar_por_id(id_alerta)
+        return alerta.to_dict() if alerta else None
 
     def inserir(self, alerta):
-
         self.repository.inserir(alerta)
 
-    def marcar_como_visualizado(self, id_alert):
+    def marcar_como_visualizado(self, id_alerta):
+        self.repository.marcar_como_visualizado(id_alerta)
 
-        self.repository.marcar_como_visualizado(
-            id_alert
-        )
-
-    def excluir(self, id_alert):
-
-        self.repository.excluir(id_alert)
+    def excluir(self, id_alerta):
+        self.repository.excluir(id_alerta)
 
     def gerar_alerta_limite(self, id_usuario):
-
         self.repository.gerar_alerta_limite(id_usuario)
